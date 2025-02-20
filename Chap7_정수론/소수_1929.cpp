5b10317621b062d1e244e07f6b7b3fa161b2694f
Jade0728 <syeong8663@gmail.com>
2025-02-20 23:20:42 +0900

    소수구하기

---
+++ Chap7_정수론/소수_1929.cpp
@@ -0,0 +1,29 @@
+#include<iostream> 
+#include<vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int m, n;
+	cin >> m >> n;
+
+	vector <bool> isPrime(n + 1, true);
+	isPrime[0] = false;
+	isPrime[1] = false;
+
+	for (int i = 2; i * i <= n; i++) { 
+		if (isPrime[i]) {
+			for (int j = i * i; j <= n; j += i) {
+				isPrime[j] = false;
+			}
+		}  
+	}
+
+	for (int i = m; i <= n; i++) {
+		if (isPrime[i]) cout << i << "\n";
+	}
+	return 0;
+}
